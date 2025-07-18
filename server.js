e2bfc704253535be61bb3815b29ec700d54b0bd3
Abstractmelon <abstractmelons@gmail.com>
2025-07-17 21:04:52 -0600

    Delete files to save space

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
-const AdmZip = require("adm-zip"); // New: For unzipping files
+const AdmZip = require("adm-zip");
 
 // face-api.js requirements
 const canvas = require("canvas");
